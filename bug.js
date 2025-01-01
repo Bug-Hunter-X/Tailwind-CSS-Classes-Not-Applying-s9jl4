aba46db847a6b6392b5ee07a01847f48f802cfff
BugHunter-X <askzenai@gmail.com>
2025-01-01 06:02:04 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,5 @@
+```javascript
+// Some Tailwind CSS classes are not applied correctly.
+// For example, the following code does not apply the `bg-red-500` class:
+<div class="bg-red-500"></div>
+```
